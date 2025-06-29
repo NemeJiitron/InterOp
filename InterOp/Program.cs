a9ebffbacf15e341930008e4b9d12bad032ad5dc
NemeJiitron <m.lukarevskiy@gmail.com>
2025-06-29 17:55:43 +0300

    ClassWork Async/ThreadPool

--- InterOp/Program.cs
+++ InterOp/Program.cs
@@ -5,6 +5,9 @@ using System.Management;
 using System.Reflection;
 using InterOp.HomeWork;
 using System.Reflection.Metadata;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using Microsoft.Data.Sqlite;
+using Dapper;
 
 namespace InterOp
 {
@@ -18,9 +21,9 @@ namespace InterOp
         public static extern int printf(string format, int i, string str);
 
         [DllImport("user32.dll", CharSet = CharSet.Unicode)]
-        public static extern int MessageBox(IntPtr nWnd, String text, string caption, uint type);
+        public static extern int MessageBox(IntPtr nWnd, string text, string caption, uint type);
         [DllImport("user32.dll", EntryPoint = "MessageBox")]
-        public static extern int MessageBoxbase(IntPtr nWnd, String text, string caption, uint type);
+        public static extern int MessageBoxbase(IntPtr nWnd, string text, string caption, uint type);
         [DllImport("user32.dll")]
         public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
         [DllImport("user32.dll")]
@@ -47,7 +50,7 @@ namespace InterOp
             NativeMethods.SendMessage(handle, 0x000C, 0, text);
         }
 
-        public static void Main()
+        public static async Task Main()
         {
             #region ClassWork 2
             //using (Process process = new Process())
@@ -155,7 +158,159 @@ namespace InterOp
             //Console.ReadKey();
             //Console.WriteLine("Thread resumed");
             #endregion
+            #region ClassWork Async/ThreadPool
+            //ThreadPool.QueueUserWorkItem(DoWork, "File 1");
+            //ThreadPool.QueueUserWorkItem(DoWork, "File 2");
 
+            //Console.ReadKey();
+
+
+            //ThreadPool.QueueUserWorkItem(DoWork, "File 1");
+            //ThreadPool.QueueUserWorkItem(DoWork, "File 2");
+            //ThreadPool.QueueUserWorkItem(DoWork, "File 3");
+            //ThreadPool.QueueUserWorkItem(DoWork, "File 4");
+
+            //Console.WriteLine($"Thread pool count: {ThreadPool.ThreadCount}");
+            //Console.WriteLine($"Thread pending pool count: {ThreadPool.PendingWorkItemCount}");
+            //Console.WriteLine($"Thread completed count: {ThreadPool.CompletedWorkItemCount}");
+
+            //Thread.Sleep(5000);
+
+            //ThreadPool.QueueUserWorkItem(DoWork, "File 5");
+            //ThreadPool.QueueUserWorkItem(DoWork, "File 6");
+            //ThreadPool.QueueUserWorkItem(DoWork, "File 7");
+            //ThreadPool.QueueUserWorkItem(DoWork, "File 8");
+
+            //Console.WriteLine($"Thread pool count: {ThreadPool.ThreadCount}");
+            //Console.WriteLine($"Thread pending pool count: {ThreadPool.PendingWorkItemCount}");
+            //Console.WriteLine($"Thread completed count: {ThreadPool.CompletedWorkItemCount}");
+
+            //Console.ReadKey();
+
+
+            //Task task = SayHelloAsync();
+
+            //Console.WriteLine($"thread {Thread.CurrentThread.ManagedThreadId}");
+
+            //await task;
+
+            //Console.WriteLine("Task finished");
+
+            //Console.ReadKey();
+
+
+            //var sw = Stopwatch.StartNew();
+
+            //Console.WriteLine($"Main запущено на потоці {Thread.CurrentThread.ManagedThreadId}");
+
+            //Task t1 = SimulateWorkAsync("Задача 1", 2000);
+            //Task t2 = SimulateWorkAsync("Задача 2", 2000);
+            //Task t3 = SimulateWorkAsync("Задача 3", 2000);
+
+            //Console.WriteLine("Усі задачі запущено паралельно");
+
+            //await Task.WhenAll(t1, t2, t3);
+
+            //Console.WriteLine($"Час (асинхронно): {sw.Elapsed.TotalSeconds} сек");
+
+            //Console.WriteLine("Усі задачі завершено");
+
+            //sw = Stopwatch.StartNew();
+
+            //await SimulateWorkAsync("Задача 1", 2000);
+            //await SimulateWorkAsync("Задача 2", 2000);
+            //await SimulateWorkAsync("Задача 3", 2000);
+
+            //Console.WriteLine($"Час (по черзі): {sw.Elapsed.TotalSeconds} сек");
+
+
+            //string connString = "Data Source=test.db";
+
+            //using var connection = new SqliteConnection(connString);
+            //await connection.OpenAsync();
+
+            //await connection.ExecuteAsync("DROP TABLE IF EXISTS Users;");
+
+            //await connection.ExecuteAsync("""
+            //    CREATE TABLE IF NOT EXISTS Users (
+            //        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+            //        Name TEXT NOT NULL,
+            //        Role TEXT NOT NULL
+            //    );
+            //""");
+
+            //// Якщо таблиця порожня — заповнюємо
+            //int count = await connection.ExecuteScalarAsync<int>("SELECT COUNT(*) FROM Users");
+            //if (count == 0)
+            //{
+            //    Console.WriteLine("Заповнюємо базу користувачами...");
+
+            //    var roles = new[] { "admin", "guest", "mod" };
+            //    var insertQuery = "INSERT INTO Users (Name, Role) VALUES (@Name, @Role)";
+
+            //    for (int i = 1; i <= 100; i++)
+            //    {
+            //        await connection.ExecuteAsync(insertQuery, new
+            //        {
+            //            Name = $"User_{i}",
+            //            Role = roles[i % roles.Length]
+            //        });
+            //    }
+
+            //    Console.WriteLine("Користувачі додані.");
+            //}
+
+            //Console.WriteLine("Паралельно завантажуємо користувачів за ролями...");
+
+            //var adminTask = connection.QueryAsync<User>("SELECT * FROM Users WHERE Role = 'admin'");
+            //var guestTask = connection.QueryAsync<User>("SELECT * FROM Users WHERE Role = 'guest'");
+            //var modTask = connection.QueryAsync<User>("SELECT * FROM Users WHERE Role = 'mod'");
+
+            //await Task.WhenAll(adminTask, guestTask, modTask);
+
+            //Console.WriteLine("\nАдміни:");
+            //foreach (var user in adminTask.Result)
+            //    Console.WriteLine($"- {user.Name}");
+
+            //Console.WriteLine("\nГості:");
+            //foreach (var user in guestTask.Result)
+            //    Console.WriteLine($"- {user.Name}");
+
+            //Console.WriteLine("\nМодератори:");
+            //foreach (var user in modTask.Result)
+            //    Console.WriteLine($"- {user.Name}");
+
+            #endregion
+
+           
+        }
+
+        
+
+        static async Task SimulateWorkAsync(string name, int delayMs)
+        {
+            Console.WriteLine($"{name} почалась (потік {Thread.CurrentThread.ManagedThreadId})");
+            await Task.Delay(delayMs);
+            Console.WriteLine($"{name} завершена (потік {Thread.CurrentThread.ManagedThreadId})");
+        }
+
+        static async Task SayHelloAsync()
+        {
+            Console.WriteLine($"Start — thread {Thread.CurrentThread.ManagedThreadId}");
+            await Task.Delay(2000);
+            Console.WriteLine($"Hello after delay — thread {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+
+        static void DoWork(object? data)
+        {
+            Console.WriteLine($"Working on: {data}. Id: {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(2000);
+            Console.WriteLine($"Work on {data} is finished");
+        }
+
+        private static void MenuEcryption()
+        {
             Console.WriteLine("Encrypting key: ");
             int key = int.Parse(Console.ReadLine());
             Dictionary<Thread, string> dict = new Dictionary<Thread, string>();
